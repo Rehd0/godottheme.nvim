268b837d10b0fc397801f0925b56d8fdd8f876df
Rehd0 <rehd070@gmail.com>
2025-11-02 21:37:29 +0100

    Update README.md LICENSE, image demos

---
+++ CONTRIBUTING.md
@@ -0,0 +1,32 @@
+
+
+# Contributing to This Project
+
+# We welcome contributions from the community! If you'd like to contribute to this project, please follow these steps:
+
+1. **Fork the Repository**: Click the "Fork" button at the top right of this page to create a copy of the repository in your own GitHub account.
+
+2. **Create a New Branch**: Open your terminal and clone your forked repository. Then, create a new branch for your feature or bug fix:
+   ```bash
+   git checkout -b feature/my-feature
+   ```
+
+3. **Make Your Changes**: Implement your changes in the codebase. Be sure to follow the existing coding style and conventions.
+
+4. **Commit Your Changes**: Once you've made your changes, stage and commit them with a descriptive message:
+   ```bash
+   git commit -am 'Add new highlight'
+   ```
+
+5. **Push to Your Branch**: Push your changes to your forked repository:
+   ```bash
+   git push origin feature/my-feature
+   ```
+
+6. **Open a Pull Request**: Go to the original repository on GitHub and click the "New Pull Request" button. Select your branch and provide a clear description of your changes.
+
+> All contributions are welcome — themes, bug fixes, or optimizations.
+
+
+# ---
+
--- lua/godot/init.lua
+++ lua/godot/init.lua
@@ -9,54 +9,53 @@ local M = {}
 function M.setup()
 
   local colors = {
-    bg = "#1D2229",
-    fg = "#DDDFE7",
-    yellow = "#F4E39A",
-    red = "#FF7A9F",
-    green = "#9AEED7",
-    darkGreen = "#6CC068",
-    typeGreen = "#5EF5CA",
-    blue = "#AAC9FE",
-    flowerBlue = "#A3A3F5",
-    funcCallBlue = "#1E90FF",
-    pink = "#FF8CCC",
-    gray = "#51545A",
-    lightGray = "#2D3238",
-    cyan = "#5ECFE7",
-    errorRed = "#613C3D",
-    errorYellow = "#3E3E32",
+    bg              = "#1D2229",
+    fg              = "#DDDFE7",
+    yellow          = "#F4E39A",
+    red             = "#FF7A9F",
+    green           = "#9AEED7",
+    darkGreen       = "#6CC068",
+    typeGreen       = "#5EF5CA",
+    blue            = "#AAC9FE",
+    flowerBlue      = "#A3A3F5",
+    funcCallBlue    = "#1E90FF",
+    pink            = "#FF8CCC",
+    gray            = "#51545A",
+    lightGray       = "#2D3238",
+    cyan            = "#5ECFE7",
+    errorRed        = "#613C3D",
+    errorYellow     = "#3E3E32",
     transparentBlue = "#3F5E7C",
-
-    lightTheme = "#353D4A",
+    lightTheme      = "#353D4A",
   }
 
   -- Define highlights
   local highlights = {
-    Normal = { fg = colors.fg, bg = colors.bg },
-    string = { fg = colors.yellow },
-    Number = { fg = colors.green },
-    Boolean = { fg = colors.red },
-    Keyword = { fg = colors.red },
-    Comment = { fg = colors.gray, italic = false, bold = false },
-    Operator = { fg = colors.blue },
-    Delimiter = { fg = colors.blue },
-    MatchParen = { bg = colors.lightGray, underline = true },
-    Statement = { fg = colors.red },
+    Normal      = { fg = colors.fg, bg = colors.bg },
+    string      = { fg = colors.yellow },
+    Number      = { fg = colors.green },
+    Boolean     = { fg = colors.red },
+    Keyword     = { fg = colors.red },
+    Comment     = { fg = colors.gray, italic = false, bold = false },
+    Operator    = { fg = colors.blue },
+    Delimiter   = { fg = colors.blue },
+    MatchParen  = { bg = colors.lightGray, underline = true },
+    Statement   = { fg = colors.red },
     Conditional = { fg = colors.pink },
-    Exception  = { fg = colors.pink },
-    Function = { fg = colors.cyan },
-    Type = { fg = colors.green },
-    -- PreProc = { fg = colors.darkGreen },
-    Call = { fg = colors.flowerBlue },
+    Exception   = { fg = colors.pink },
+    Function    = { fg = colors.cyan },
+    Type        = { fg = colors.green },
+    PreProc     = { fg = colors.darkGreen },
+    Call        = { fg = colors.flowerBlue },
 
     DiagnosticError = { fg = "#EE0000", bg = colors.errorRed, bold = false },
-    DiagnosticWarn = { fg = "#EEEE00", bg = colors.errorYellow, bold = false },
+    DiagnosticWarn  = { fg = "#EEEE00", bg = colors.errorYellow, bold = false },
 
     CursorLine = { bg = colors.lightGray },
 
-    Visual = { bg = colors.transparentBlue },
-    TabLine = { fg = "#666D75", bg = "#242B34" },
-    TabLineSel = { bg = colors.lightTheme },
+    Visual       = { bg = colors.transparentBlue },
+    TabLine      = { fg = "#666D75", bg = "#242B34" },
+    TabLineSel   = { bg = colors.lightTheme },
     WinSeparator = { fg = "#476E8F" },
 
     Search = { bg = colors.transparentBlue },
@@ -89,7 +88,9 @@ function M.setup()
     ["@keyword.return"]      = { fg = colors.pink },
     ["@function.call"]       = { fg = colors.funcCallBlue },
 
+    -- ==================================================
     -- HTML / CSS PHP XNL
+    -- ==================================================
     Tag = { fg = colors.red },
     ["@htmlLink"] = { fg = colors.errorYellow },
     ["@tag.delimiter"]          = { fg = colors.blue },
@@ -98,7 +99,7 @@ function M.setup()
     ["@tag.property"] = { fg = colors.flowerBlue },
 
     -- Treesitter context
-    TreesitterContext         = { fg = colors.fg, bg = colors.lightGray },
+    TreesitterContext           = { fg = colors.fg, bg = colors.lightGray },
     TreesitterContextLineNumber = { fg = colors.gray, bg = colors.lightGray },
 
     -- indent Blank line
